793e6aa4f3c637032612d59608e916204520d06f
Andrey Svyazhin <andreysvyazhin@Andreys-MacBook-Pro.local>
2018-03-02 19:27:55 +0300

    Delete files

--- utils/packagesMerge.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-
-const localPackageJson = require('../../../package.json')
-const sharedPackageJson = require('../package.json')
-
-Object.assign(localPackageJson.dependencies, sharedPackageJson.dependencies)
-
-fs.writeFileSync('../../../package.json', JSON.stringify(localPackageJson, null, 2))
