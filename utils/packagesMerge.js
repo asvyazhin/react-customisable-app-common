2145e647b6434db8d2dac732691a76b19e050069
Andrey Svyazhin <andreysvyazhin@Andreys-MacBook-Pro.local>
2018-03-02 18:02:20 +0300

    add files

--- utils/packagesMerge.js
+++ utils/packagesMerge.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
 
-const localPackageJson = require('../../package.json')
+const localPackageJson = require('../../../package.json')
 const sharedPackageJson = require('../package.json')
 
 Object.assign(localPackageJson.dependencies, sharedPackageJson.dependencies)
 
-fs.writeFileSync('../../package.json', JSON.stringify(localPackageJson, null, 2))
+fs.writeFileSync('../../../package.json', JSON.stringify(localPackageJson, null, 2))
